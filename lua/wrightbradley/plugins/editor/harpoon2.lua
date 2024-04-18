3ac9dd37b0711110cdfbdc1139164d3a1fac1155
wrightbradley <bradley.wright@tackle.io>
2024-04-18 14:49:14 -0400

    feat: add keybinding enhancements for harpoon2
    Signed-off-by: wrightbradley <bradley.wright@tackle.io>

--- lua/wrightbradley/plugins/editor/harpoon2.lua
+++ lua/wrightbradley/plugins/editor/harpoon2.lua
@@ -2,22 +2,17 @@ return {
   {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
-    opts = {
-      menu = {
-        width = vim.api.nvim_win_get_width(0) - 4,
-      },
-    },
     keys = function()
       local keys = {
         {
-          "<leader>H",
+          "<leader>ha",
           function()
-            require("harpoon"):list():append()
+            require("harpoon"):list():add()
           end,
           desc = "Harpoon File",
         },
         {
-          "<leader>h",
+          "<leader>hl",
           function()
             local harpoon = require("harpoon")
             harpoon.ui:toggle_quick_menu(harpoon:list())
@@ -25,14 +20,51 @@ return {
           desc = "Harpoon Quick Menu",
         },
       }
+      table.insert(keys, {
+        "<leader>ht",
+        function()
+          local harpoon = require("harpoon")
+          -- basic telescope configuration
+          local conf = require("telescope.config").values
+          local function toggle_telescope(harpoon_files)
+            local file_paths = {}
+            for _, item in ipairs(harpoon_files.items) do
+              table.insert(file_paths, item.value)
+            end
+
+            require("telescope.pickers")
+              .new({}, {
+                prompt_title = "Harpoon",
+                finder = require("telescope.finders").new_table({
+                  results = file_paths,
+                }),
+                previewer = conf.file_previewer({}),
+                sorter = conf.generic_sorter({}),
+              })
+              :find()
+          end
+          toggle_telescope(harpoon:list())
+        end,
+        desc = "Harpoon Telescope",
+      })
 
       for i = 1, 5 do
         table.insert(keys, {
-          "<leader>" .. i,
+          "<leader>h" .. i,
           function()
             require("harpoon"):list():select(i)
           end,
           desc = "Harpoon to File " .. i,
+        })
+      end
+
+      for i = 1, 5 do
+        table.insert(keys, {
+          "<leader>hr" .. i,
+          function()
+            require("harpoon"):list():select(i)
+          end,
+          desc = "Harpoon replace File at " .. i,
         })
       end
       return keys
