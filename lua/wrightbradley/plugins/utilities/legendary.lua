a24cd0935e1ba753d3be3c320c345b8e0c8eeeb2
wrightbradley <bradley.wright@tackle.io>
2024-04-18 07:28:35 -0400

    feat: add legendary plugin
    Signed-off-by: wrightbradley <bradley.wright@tackle.io>

--- lua/wrightbradley/plugins/utilities/legendary.lua
+++ lua/wrightbradley/plugins/utilities/legendary.lua
@@ -3,9 +3,29 @@ return {
     "mrjones2014/legendary.nvim",
     -- since legendary.nvim handles all your keymaps/commands,
     -- its recommended to load legendary.nvim before other plugins
-    priority = 1000,
+    priority = 10000,
     lazy = false,
     -- sqlite is only needed if you want to use frecency sorting
     -- dependencies = { 'kkharji/sqlite.lua' }
+    opts = {
+      extensions = {
+        lazy_nvim = {
+          -- Automatically register keymaps that are defined on lazy.nvim plugin specs
+          -- using the `keys = {}` property.
+          auto_register = true,
+        },
+        which_key = {
+          -- Automatically add which-key tables to legendary
+          -- see ./doc/WHICH_KEY.md for more details
+          auto_register = true,
+        },
+      },
+    },
+    config = function(_, opts)
+      require("legendary").setup(opts)
+      --   require("todo-comments").setup(opts)
+      --   local mappings = require("lazy-plugins.keymaps.todo_comments")
+      --   require("legendary").keymaps(mappings)
+    end,
   },
 }
