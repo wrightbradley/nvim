eb92c8a49ff4a74c462aafd9eb4fc80ca1db23f9
wrightbradley <bradley.wright@tackle.io>
2024-04-19 14:27:27 -0400

    feat: enable copilot
    Signed-off-by: wrightbradley <bradley.wright@tackle.io>

--- lua/wrightbradley/plugins/coding/copilot.lua
+++ lua/wrightbradley/plugins/coding/copilot.lua
@@ -1,6 +1,3 @@
-if true then
-  return {}
-end
 -- copilot suggestions and completion
 return {
   -- copilot
@@ -54,7 +51,96 @@ return {
       })
     end,
   },
-
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    keys = {
+      -- Code related commands
+      { "<leader>cce", "<Cmd>CopilotChatExplain<CR>", desc = "CopilotChat - Explain code" },
+      { "<leader>cct", "<Cmd>CopilotChatTests<CR>", desc = "CopilotChat - Generate tests" },
+      { "<leader>ccr", "<Cmd>CopilotChatReview<CR>", desc = "CopilotChat - Review code" },
+      { "<leader>ccR", "<Cmd>CopilotChatRefactor<CR>", desc = "CopilotChat - Refactor code" },
+      { "<leader>ccn", "<Cmd>CopilotChatBetterNamings<CR>", desc = "CopilotChat - Better Naming" },
+      -- Chat with Copilot in visual mode
+      { "<leader>ccv", ":CopilotChatVisual ", mode = "x", desc = "CopilotChat - Chat with vertical split" },
+      { "<leader>ccx", ":CopilotChatInline<CR>", mode = "x", desc = "CopilotChat - Inline chat" },
+      -- Custom input for CopilotChat
+      {
+        "<leader>cci",
+        function()
+          local input = vim.fn.input("Ask Copilot: ")
+          if input ~= "" then
+            vim.cmd("CopilotChat " .. input)
+          end
+        end,
+        desc = "CopilotChat - Ask input",
+      },
+      -- Generate commit message based on the git diff
+      { "<leader>ccm", "<Cmd>CopilotChatCommit<CR>", desc = "CopilotChat - Generate commit message for all changes" },
+      {
+        "<leader>ccM",
+        "<Cmd>CopilotChatCommitStaged<CR>",
+        desc = "CopilotChat - Generate commit message for staged changes",
+      },
+      -- Quick chat with Copilot
+      {
+        "<leader>ccq",
+        function()
+          local input = vim.fn.input("Quick Chat: ")
+          if input ~= "" then
+            vim.cmd("CopilotChatBuffer " .. input)
+          end
+        end,
+        desc = "CopilotChat - Quick chat",
+      },
+      -- Debug
+      { "<leader>ccd", "<Cmd>CopilotChatDebugInfo<CR>", desc = "CopilotChat - Debug Info" },
+      -- Fix the issue with diagnostic
+      { "<leader>ccf", "<Cmd>CopilotChatFixDiagnostic<CR>", desc = "CopilotChat - Fix Diagnostic" },
+      -- Clear buffer and chat history
+      { "<leader>ccl", "<Cmd>CopilotChatReset<CR>", desc = "CopilotChat - Clear buffer and chat history" },
+      --  -- Toggle Copilot Chat Vsplit
+      --  { "<leader>ccv", "<Cmd>CopilotChatToggle<CR>", desc = "CopilotChat - Toggle Vsplit" },
+      -- Show help actions with telescope
+      {
+        "<leader>cch",
+        function()
+          local actions = require("CopilotChat.actions")
+          require("CopilotChat.integrations.telescope").pick(actions.help_actions())
+        end,
+        desc = "CopilotChat - Help actions",
+      }, -- show help actions with telescope
+      {
+        "<leader>ccp",
+        function()
+          local actions = require("CopilotChat.actions")
+          require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+        end,
+        desc = "CopilotChat - Help actions",
+      }, -- show prompts actions with telescope
+      {
+        "<leader>ccp",
+        ":lua require('CopilotChat.integrations.telescope').pick(require('CopilotChat.actions').prompt_actions())<CR>",
+        desc = "CopilotChat - Prompt actions",
+      },
+    },
+    opts = {
+      debug = true, -- Enable debugging
+      window = {
+        layout = "float",
+        relative = "cursor",
+        width = 1,
+        height = 0.4,
+        row = 1,
+      },
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
   -- copilot cmp source
   {
     "nvim-cmp",
