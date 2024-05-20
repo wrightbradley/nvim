16e3406de1bdf92c615edf14ace3dd7e8ec6bfca
wrightbradley <bradley.wright.tech@gmail.com>
2024-05-20 10:55:27 -0400

    feat: update plugins for cmake/rust
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/lang/cmake.lua
+++ lua/wrightbradley/plugins/lang/cmake.lua
@@ -7,16 +7,6 @@ return {
       end
     end,
   },
-  -- {
-  --   "nvimtools/none-ls.nvim",
-  --   optional = true,
-  --   opts = function(_, opts)
-  --     local nls = require("null-ls")
-  --     opts.sources = vim.list_extend(opts.sources or {}, {
-  --       nls.builtins.diagnostics.cmake_lint,
-  --     })
-  --   end,
-  -- },
   {
     "mfussenegger/nvim-lint",
     optional = true,
