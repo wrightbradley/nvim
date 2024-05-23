7f98c77c9c1767bfdd86bcf3ac4259c3a7c7f2a1
wrightbradley <bradley.wright.tech@gmail.com>
2024-05-23 11:41:30 -0400

    feat: configure markdown lang
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/lang/markdown.lua
+++ lua/wrightbradley/plugins/lang/markdown.lua
@@ -1,5 +1,15 @@
 return {
   {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ["markdown"] = { { "prettierd", "prettier" }, "markdownlint", "markdown-toc" },
+        ["markdown.mdx"] = { { "prettierd", "prettier" }, "markdownlint", "markdown-toc" },
+      },
+    },
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       if type(opts.ensure_installed) == "table" then
@@ -14,16 +24,6 @@ return {
       vim.list_extend(opts.ensure_installed, { "markdownlint", "marksman", "vale-ls", "vale" })
     end,
   },
-  -- {
-  --   "nvimtools/none-ls.nvim",
-  --   optional = true,
-  --   opts = function(_, opts)
-  --     local nls = require("null-ls")
-  --     opts.sources = vim.list_extend(opts.sources or {}, {
-  --       nls.builtins.diagnostics.markdownlint,
-  --     })
-  --   end,
-  -- },
   {
     "mfussenegger/nvim-lint",
     optional = true,
@@ -62,32 +62,31 @@ return {
       vim.cmd([[do FileType]])
     end,
   },
-
-  -- {
-  --   "lukas-reineke/headlines.nvim",
-  --   opts = function()
-  --     local opts = {}
-  --     for _, ft in ipairs({ "markdown", "norg", "rmd", "org" }) do
-  --       opts[ft] = {
-  --         headline_highlights = {},
-  --         -- disable bullets for now. See https://github.com/lukas-reineke/headlines.nvim/issues/66
-  --         bullets = {},
-  --       }
-  --       for i = 1, 6 do
-  --         local hl = "Headline" .. i
-  --         vim.api.nvim_set_hl(0, hl, { link = "Headline", default = true })
-  --         table.insert(opts[ft].headline_highlights, hl)
-  --       end
-  --     end
-  --     return opts
-  --   end,
-  --   ft = { "markdown", "norg", "rmd", "org" },
-  --   config = function(_, opts)
-  --     -- PERF: schedule to prevent headlines slowing down opening a file
-  --     vim.schedule(function()
-  --       require("headlines").setup(opts)
-  --       require("headlines").refresh()
-  --     end)
-  --   end,
-  -- },
+  {
+    "lukas-reineke/headlines.nvim",
+    opts = function()
+      local opts = {}
+      for _, ft in ipairs({ "markdown", "norg", "rmd", "org" }) do
+        opts[ft] = {
+          headline_highlights = {},
+          -- disable bullets for now. See https://github.com/lukas-reineke/headlines.nvim/issues/66
+          bullets = {},
+        }
+        for i = 1, 6 do
+          local hl = "Headline" .. i
+          vim.api.nvim_set_hl(0, hl, { link = "Headline", default = true })
+          table.insert(opts[ft].headline_highlights, hl)
+        end
+      end
+      return opts
+    end,
+    ft = { "markdown", "norg", "rmd", "org" },
+    config = function(_, opts)
+      -- PERF: schedule to prevent headlines slowing down opening a file
+      vim.schedule(function()
+        require("headlines").setup(opts)
+        require("headlines").refresh()
+      end)
+    end,
+  },
 }
