21e430c02d8f4e27b3ccc74dee0806a765ab9971
wrightbradley <bradley.wright.tech@gmail.com>
2024-05-23 11:22:16 -0400

    feat: configure mini-hipatterns
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/utilities/mini-hipatterns.lua
+++ lua/wrightbradley/plugins/utilities/mini-hipatterns.lua
@@ -5,7 +5,8 @@ M.hl = {}
 
 M.plugin = {
   "echasnovski/mini.hipatterns",
-  event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+  desc = "Highlight colors in your code. Also includes Tailwind CSS support.",
+  event = "LazyFile",
   opts = function()
     local hi = require("mini.hipatterns")
     return {
@@ -19,6 +20,18 @@ M.plugin = {
       },
       highlighters = {
         hex_color = hi.gen_highlighter.hex_color({ priority = 2000 }),
+        shorthand = {
+          pattern = "()#%x%x%x()%f[^%x%w]",
+          group = function(_, _, data)
+            ---@type string
+            local match = data.full_match
+            local r, g, b = match:sub(2, 2), match:sub(3, 3), match:sub(4, 4)
+            local hex_color = "#" .. r .. r .. g .. g .. b .. b
+
+            return MiniHipatterns.compute_hex_color_group(hex_color, "bg")
+          end,
+          extmark_opts = { priority = 2000 },
+        },
       },
     }
   end,
