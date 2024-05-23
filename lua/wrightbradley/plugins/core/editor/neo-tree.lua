cba6486d8b054bc43328fd7e16d9b77fdbe53b93
wrightbradley <bradley.wright.tech@gmail.com>
2024-05-23 10:39:14 -0400

    feat: configure neo-tree settings
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/core/editor/neo-tree.lua
+++ lua/wrightbradley/plugins/core/editor/neo-tree.lua
@@ -40,16 +40,26 @@ return {
       vim.cmd([[Neotree close]])
     end,
     init = function()
-      if vim.fn.argc(-1) == 1 then
-        local stat = vim.uv.fs_stat(vim.fn.argv(0))
-        if stat and stat.type == "directory" then
-          require("neo-tree")
-        end
-      end
+      -- FIX: use `autocmd` for lazy-loading neo-tree instead of directly requiring it,
+      -- because `cwd` is not set up properly.
+      vim.api.nvim_create_autocmd("BufEnter", {
+        group = vim.api.nvim_create_augroup("Neotree_start_directory", { clear = true }),
+        desc = "Start Neo-tree with directory",
+        once = true,
+        callback = function()
+          if package.loaded["neo-tree"] then
+            return
+          else
+            local stats = vim.uv.fs_stat(vim.fn.argv(0))
+            if stats and stats.type == "directory" then
+              require("neo-tree")
+            end
+          end
+        end,
+      })
     end,
     opts = {
-      -- sources = { "filesystem", "buffers", "git_status", "document_symbols" },
-      sources = { "filesystem", "document_symbols" },
+      sources = { "filesystem", "buffers", "git_status", "document_symbols" },
       open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
       filesystem = {
         bind_to_cwd = false,
@@ -69,7 +79,7 @@ return {
           },
           ["O"] = {
             function(state)
-              Util.open(state.tree:get_node().path, { system = true })
+              require("lazy.util").open(state.tree:get_node().path, { system = true })
             end,
             desc = "Open with System Application",
           },
@@ -81,6 +91,12 @@ return {
           expander_collapsed = "",
           expander_expanded = "",
           expander_highlight = "NeoTreeExpander",
+        },
+        git_status = {
+          symbols = {
+            unstaged = "󰄱",
+            staged = "󰱒",
+          },
         },
       },
     },
