e9054ec91c6b88bc1d1aa0de00c744bee30becde
wrightbradley <bradley.wright.tech@gmail.com>
2024-05-23 11:43:15 -0400

    feat: configure typescript lang
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/lang/typescript.lua
+++ lua/wrightbradley/plugins/lang/typescript.lua
@@ -1,5 +1,15 @@
-return {
+local inlay_hints_settings = {
+  includeInlayEnumMemberValueHints = true,
+  includeInlayFunctionLikeReturnTypeHints = true,
+  includeInlayFunctionParameterTypeHints = true,
+  includeInlayParameterNameHints = "literal",
+  includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+  includeInlayPropertyDeclarationTypeHints = true,
+  includeInlayVariableTypeHints = false,
+  includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+}
 
+return {
   -- add typescript to treesitter
   {
     "nvim-treesitter/nvim-treesitter",
@@ -46,8 +56,13 @@ return {
               desc = "Remove Unused Imports",
             },
           },
-          ---@diagnostic disable-next-line: missing-fields
           settings = {
+            typescript = {
+              inlayHints = inlay_hints_settings,
+            },
+            javascript = {
+              inlayHints = inlay_hints_settings,
+            },
             completions = {
               completeFunctionCalls = true,
             },
@@ -87,6 +102,20 @@ return {
           },
         }
       end
+      if not dap.adapters["node"] then
+        dap.adapters["node"] = function(cb, config)
+          if config.type == "node" then
+            config.type = "pwa-node"
+          end
+          local nativeAdapter = dap.adapters["pwa-node"]
+          if type(nativeAdapter) == "function" then
+            nativeAdapter(cb, config)
+          else
+            cb(nativeAdapter)
+          end
+        end
+      end
+
       for _, language in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact" }) do
         if not dap.configurations[language] then
           dap.configurations[language] = {
