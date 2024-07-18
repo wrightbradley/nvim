f330d85de28f345b7e74d5872071370e6f2710d1
wrightbradley <bradley.wright.tech@gmail.com>
2024-07-18 09:37:55 -0400

    feat: update util/toggle
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/util/toggle.lua
+++ lua/wrightbradley/util/toggle.lua
@@ -1,94 +1,185 @@
 ---@class wrightbradley.util.toggle
 local M = {}
 
----@param silent boolean?
----@param values? {[1]:any, [2]:any}
-function M.option(option, silent, values)
-  if values then
-    if vim.opt_local[option]:get() == values[1] then
-      ---@diagnostic disable-next-line: no-unknown
-      vim.opt_local[option] = values[2]
-    else
-      ---@diagnostic disable-next-line: no-unknown
-      vim.opt_local[option] = values[1]
-    end
-    return Util.info("Set " .. option .. " to " .. vim.opt_local[option]:get(), { title = "Option" })
+---@class wrightbradley.Toggle
+---@field name string
+---@field get fun():boolean
+---@field set fun(state:boolean)
+
+---@class wrightbradley.Toggle.wrap: wrightbradley.Toggle
+---@operator call:boolean
+
+---@param toggle wrightbradley.Toggle
+function M.wrap(toggle)
+  return setmetatable(toggle, {
+    __call = function()
+      toggle.set(not toggle.get())
+      local state = toggle.get()
+      if state then
+        Util.info("Enabled " .. toggle.name, { title = toggle.name })
+      else
+        Util.warn("Disabled " .. toggle.name, { title = toggle.name })
+      end
+      return state
+    end,
+  }) --[[@as wrightbradley.Toggle.wrap]]
+end
+
+---@param lhs string
+---@param toggle wrightbradley.Toggle
+function M.map(lhs, toggle)
+  local t = M.wrap(toggle)
+  Util.safe_keymap_set("n", lhs, function()
+    t()
+  end, { desc = "Toggle " .. toggle.name })
+  M.wk(lhs, toggle)
+end
+
+function M.wk(lhs, toggle)
+  if not Util.has("which-key.nvim") then
+    return
   end
-  ---@diagnostic disable-next-line: no-unknown
-  vim.opt_local[option] = not vim.opt_local[option]:get()
-  if not silent then
-    if vim.opt_local[option]:get() then
-      Util.info("Enabled " .. option, { title = "Option" })
-    else
-      Util.warn("Disabled " .. option, { title = "Option" })
+  local function safe_get()
+    local ok, enabled = pcall(toggle.get)
+    if not ok then
+      Util.error({ "Failed to get toggle state for **" .. toggle.name .. "**:\n", enabled }, { once = true })
     end
+    return enabled
   end
+  require("which-key").add({
+    {
+      lhs,
+      icon = function()
+        return safe_get() and { icon = " ", color = "green" } or { icon = " ", color = "yellow" }
+      end,
+      desc = function()
+        return (safe_get() and "Disable " or "Enable ") .. toggle.name
+      end,
+    },
+  })
+end
+
+M.treesitter = M.wrap({
+  name = "Treesitter Highlight",
+  get = function()
+    return vim.b.ts_highlight
+  end,
+  set = function(state)
+    if state then
+      vim.treesitter.start()
+    else
+      vim.treesitter.stop()
+    end
+  end,
+})
+
+---@param buf? boolean
+function M.format(buf)
+  return M.wrap({
+    name = "Auto Format (" .. (buf and "Buffer" or "Global") .. ")",
+    get = function()
+      if not buf then
+        return vim.g.autoformat == nil or vim.g.autoformat
+      end
+      return Util.format.enabled()
+    end,
+    set = function(state)
+      Util.format.enable(state, buf)
+    end,
+  })
+end
+
+---@param opts? {values?: {[1]:any, [2]:any}, name?: string}
+function M.option(option, opts)
+  opts = opts or {}
+  local name = opts.name or option
+  local on = opts.values and opts.values[2] or true
+  local off = opts.values and opts.values[1] or false
+  return M.wrap({
+    name = name,
+    get = function()
+      return vim.opt_local[option]:get() == on
+    end,
+    set = function(state)
+      vim.opt_local[option] = state and on or off
+    end,
+  })
 end
 
 local nu = { number = true, relativenumber = true }
-function M.number()
-  if vim.opt_local.number:get() or vim.opt_local.relativenumber:get() then
-    nu = { number = vim.opt_local.number:get(), relativenumber = vim.opt_local.relativenumber:get() }
-    vim.opt_local.number = false
-    vim.opt_local.relativenumber = false
-    Util.warn("Disabled line numbers", { title = "Option" })
-  else
-    vim.opt_local.number = nu.number
-    vim.opt_local.relativenumber = nu.relativenumber
-    Util.info("Enabled line numbers", { title = "Option" })
-  end
-end
-
-local enabled = true
-function M.diagnostics()
-  -- if this Neovim version supports checking if diagnostics are enabled
-  -- then use that for the current state
-  if vim.diagnostic.is_enabled then
-    enabled = vim.diagnostic.is_enabled()
-  elseif vim.diagnostic.is_disabled then
-    enabled = not vim.diagnostic.is_disabled()
-  end
-  enabled = not enabled
-
-  if enabled then
-    vim.diagnostic.enable()
-    Util.info("Enabled diagnostics", { title = "Diagnostics" })
-  else
-    vim.diagnostic.disable()
-    Util.warn("Disabled diagnostics", { title = "Diagnostics" })
-  end
-end
-
----@param buf? number
----@param value? boolean
-function M.inlay_hints(buf, value)
-  local ih = vim.lsp.buf.inlay_hint or vim.lsp.inlay_hint
-  if type(ih) == "function" then
-    ih(buf, value)
-  elseif type(ih) == "table" and ih.enable then
-    if value == nil then
-      value = not ih.is_enabled({ bufnr = buf or 0 })
+M.number = M.wrap({
+  name = "Line Numbers",
+  get = function()
+    return vim.opt_local.number:get() or vim.opt_local.relativenumber:get()
+  end,
+  set = function(state)
+    if state then
+      vim.opt_local.number = nu.number
+      vim.opt_local.relativenumber = nu.relativenumber
+    else
+      nu = { number = vim.opt_local.number:get(), relativenumber = vim.opt_local.relativenumber:get() }
+      vim.opt_local.number = false
+      vim.opt_local.relativenumber = false
     end
-    ih.enable(value, { bufnr = buf })
-  end
-end
+  end,
+})
 
+M.diagnostics = M.wrap({
+  name = "Diagnostics",
+  get = function()
+    return vim.diagnostic.is_enabled and vim.diagnostic.is_enabled()
+  end,
+  set = vim.diagnostic.enable,
+})
+
+M.inlay_hints = M.wrap({
+  name = "Inlay Hints",
+  get = function()
+    return vim.lsp.inlay_hint.is_enabled({ bufnr = 0 })
+  end,
+  set = function(state)
+    vim.lsp.inlay_hint.enable(state, { bufnr = 0 })
+  end,
+})
+
+---@type {k:string, v:any}[]
 M._maximized = nil
-function M.maximize()
-  if M._maximized then
-    vim.o.winwidth = M._maximized.width
-    vim.o.winheight = M._maximized.height
-    M._maximized = nil
-    vim.cmd("wincmd =")
-  else
-    M._maximized = {
-      width = vim.o.winwidth,
-      height = vim.o.winheight,
-    }
-    vim.o.winwidth = 999
-    vim.o.winheight = 999
-  end
-end
+M.maximize = M.wrap({
+  name = "Maximize",
+  get = function()
+    return M._maximized ~= nil
+  end,
+  set = function(state)
+    if state then
+      M._maximized = {}
+      local function set(k, v)
+        table.insert(M._maximized, 1, { k = k, v = vim.o[k] })
+        vim.o[k] = v
+      end
+      set("winwidth", 999)
+      set("winheight", 999)
+      set("winminwidth", 10)
+      set("winminheight", 4)
+      vim.cmd("wincmd =")
+      -- `QuitPre` seems to be executed even if we quit a normal window, so we don't want that
+      -- `VimLeavePre` might be another consideration? Not sure about differences between the 2
+      vim.api.nvim_create_autocmd("ExitPre", {
+        once = true,
+        group = vim.api.nvim_create_augroup("wrightbradley_restore_max_exit_pre", { clear = true }),
+        desc = "Restore width/height when close Neovim while maximized",
+        callback = function()
+          M.maximize.set(false)
+        end,
+      })
+    else
+      for _, opt in ipairs(M._maximized) do
+        vim.o[opt.k] = opt.v
+      end
+      M._maximized = nil
+      vim.cmd("wincmd =")
+    end
+  end,
+})
 
 setmetatable(M, {
   __call = function(m, ...)
