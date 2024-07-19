1175d29adc228e70923fae83017cdb066718e3cd
wrightbradley <bradley.wright.tech@gmail.com>
2024-07-19 09:04:59 -0400

    feat: move to gpt4o-mini and add keymaps
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/coding/chatgpt.lua
+++ lua/wrightbradley/plugins/coding/chatgpt.lua
@@ -7,7 +7,7 @@ return {
       require("chatgpt").setup({
         api_key_cmd = "gpg --decrypt " .. home .. "/chatgpt.key.gpg",
         openai_params = {
-          model = "gpt-4-1106-preview",
+          model = "gpt-4o-mini",
           frequency_penalty = 0,
           presence_penalty = 0,
           max_tokens = 4096,
@@ -16,7 +16,7 @@ return {
           n = 1,
         },
         openai_edit_params = {
-          model = "gpt-4-1106-preview",
+          model = "gpt-4o-mini",
           frequency_penalty = 0,
           presence_penalty = 0,
           temperature = 0,
@@ -29,6 +29,27 @@ return {
       "MunifTanjim/nui.nvim",
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope.nvim",
+    },
+    keys = {
+      { "<leader>G", desc = "ChatGPT" },
+      { "<leader>Gc", "<cmd>ChatGPT<CR>", desc = "ChatGPT" },
+      { "<leader>Gg", "<cmd>ChatGPTRun grammar_correction<CR>", desc = "Grammar Correction", mode = { "n", "v" } },
+      { "<leader>Ge", "<cmd>ChatGPTEditWithInstruction<CR>", desc = "Edit with instruction", mode = { "n", "v" } },
+      { "<leader>Gt", "<cmd>ChatGPTRun translate<CR>", desc = "Translate", mode = { "n", "v" } },
+      { "<leader>Gk", "<cmd>ChatGPTRun keywords<CR>", desc = "Keywords", mode = { "n", "v" } },
+      { "<leader>Gd", "<cmd>ChatGPTRun docstring<CR>", desc = "Docstring", mode = { "n", "v" } },
+      { "<leader>Ga", "<cmd>ChatGPTRun add_tests<CR>", desc = "Add Tests", mode = { "n", "v" } },
+      { "<leader>Go", "<cmd>ChatGPTRun optimize_code<CR>", desc = "Optimize Code", mode = { "n", "v" } },
+      { "<leader>Gs", "<cmd>ChatGPTRun summarize<CR>", desc = "Summarize", mode = { "n", "v" } },
+      { "<leader>Gf", "<cmd>ChatGPTRun fix_bugs<CR>", desc = "Fix Bugs", mode = { "n", "v" } },
+      { "<leader>Gx", "<cmd>ChatGPTRun explain_code<CR>", desc = "Explain Code", mode = { "n", "v" } },
+      { "<leader>Gr", "<cmd>ChatGPTRun roxygen_edit<CR>", desc = "Roxygen Edit", mode = { "n", "v" } },
+      {
+        "<leader>Gl",
+        "<cmd>ChatGPTRun code_readability_analysis<CR>",
+        desc = "Code Readability Analysis",
+        mode = { "n", "v" },
+      },
     },
   },
 }
