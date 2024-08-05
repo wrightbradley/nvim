8eb58cbab7186e57e07735f15b9d044f2762b1e8
wrightbradley <bradley.wright.tech@gmail.com>
2024-08-05 08:21:33 -0400

    feat: updating coding/mini-pairs
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/coding/mini-pairs.lua
+++ lua/wrightbradley/plugins/coding/mini-pairs.lua
@@ -7,23 +7,19 @@ return {
     "echasnovski/mini.pairs",
     event = "VeryLazy",
     opts = {
-      mappings = {
-        ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\`].", register = { cr = false } },
-      },
+      modes = { insert = true, command = true, terminal = false },
+      -- skip autopair when next character is one of these
+      skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+      -- skip autopair when the cursor is inside these treesitter nodes
+      skip_ts = { "string" },
+      -- skip autopair when next character is closing pair
+      -- and there are more closing pairs than opening pairs
+      skip_unbalanced = true,
+      -- better deal with markdown code blocks
+      markdown = true,
     },
-    keys = {
-      {
-        "<leader>up",
-        function()
-          vim.g.minipairs_disable = not vim.g.minipairs_disable
-          if vim.g.minipairs_disable then
-            Util.warn("Disabled auto pairs", { title = "Option" })
-          else
-            Util.info("Enabled auto pairs", { title = "Option" })
-          end
-        end,
-        desc = "Toggle Auto Pairs",
-      },
-    },
+    config = function(_, opts)
+      Util.mini.pairs(opts)
+    end,
   },
 }
