2d526b263ced234098f44c07494f805d2ea03a8e
wrightbradley <bradley.wright.tech@gmail.com>
2024-08-08 15:04:04 -0400

    feat: update elixer
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/elixir.lua
+++ lua/wrightbradley/plugins/elixir.lua
@@ -1,21 +1,40 @@
 return {
   {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "elixir",
-        "heex",
-        "eex",
-      })
-    end,
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        elixirls = {
+          keys = {
+            {
+              "<leader>cp",
+              function()
+                local params = vim.lsp.util.make_position_params()
+                Util.lsp.execute({
+                  command = "manipulatePipes:serverid",
+                  arguments = { "toPipe", params.textDocument.uri, params.position.line, params.position.character },
+                })
+              end,
+              desc = "To Pipe",
+            },
+            {
+              "<leader>cP",
+              function()
+                local params = vim.lsp.util.make_position_params()
+                Util.lsp.execute({
+                  command = "manipulatePipes:serverid",
+                  arguments = { "fromPipe", params.textDocument.uri, params.position.line, params.position.character },
+                })
+              end,
+              desc = "From Pipe",
+            },
+          },
+        },
+      },
+    },
   },
   {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "elixir-ls",
-      })
-    end,
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "elixir", "heex", "eex" } },
   },
   {
     "nvim-neotest/neotest",
@@ -30,14 +49,33 @@ return {
     },
   },
   {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = vim.list_extend(opts.sources or {}, {
+        nls.builtins.diagnostics.credo.with({
+          condition = function(utils)
+            return utils.root_has_file(".credo.exs")
+          end,
+        }),
+      })
+    end,
+  },
+  {
     "mfussenegger/nvim-lint",
     optional = true,
     opts = function(_, opts)
-      if vim.fn.executable("credo") == 0 then
-        return
-      end
       opts.linters_by_ft = {
         elixir = { "credo" },
+      }
+
+      opts.linters = {
+        credo = {
+          condition = function(ctx)
+            return vim.fs.find({ ".credo.exs" }, { path = ctx.filename, upward = true })[1]
+          end,
+        },
       }
     end,
   },
