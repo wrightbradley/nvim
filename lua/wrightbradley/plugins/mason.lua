28191f75e2cec80e27d68ec1c0ded3899eef900f
wrightbradley <bradley.wright.tech@gmail.com>
2024-08-09 09:03:18 -0400

    feat: update plugins/mason
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/mason.lua
+++ lua/wrightbradley/plugins/mason.lua
@@ -1,11 +1,11 @@
 return {
   -- cmdline tools and lsp servers
   {
-
     "williamboman/mason.nvim",
     cmd = "Mason",
     keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
     build = ":MasonUpdate",
+    opts_extend = { "ensure_installed" },
     opts = {
       ensure_installed = {
         "stylua",
@@ -38,19 +38,15 @@ return {
           })
         end, 100)
       end)
-      local function ensure_installed()
+
+      mr.refresh(function()
         for _, tool in ipairs(opts.ensure_installed) do
           local p = mr.get_package(tool)
           if not p:is_installed() then
             p:install()
           end
         end
-      end
-      if mr.refresh then
-        mr.refresh(ensure_installed)
-      else
-        ensure_installed()
-      end
+      end)
     end,
   },
 }
