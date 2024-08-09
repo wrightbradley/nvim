69b060d6e7b33fd25f479a668513cfd56e43531d
wrightbradley <bradley.wright.tech@gmail.com>
2024-08-09 09:04:30 -0400

    feat: update plugins/nvim-cmp
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/nvim-cmp.lua
+++ lua/wrightbradley/plugins/nvim-cmp.lua
@@ -22,19 +22,19 @@ return {
       vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
       local cmp = require("cmp")
       local defaults = require("cmp.config.default")()
+      local auto_select = true
       return {
         auto_brackets = {}, -- configure any filetype to auto add brackets
         completion = {
-          completeopt = "menu,menuone,noinsert",
+          completeopt = "menu,menuone,noinsert" .. (auto_select and "" or ",noselect"),
         },
+        preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None,
         mapping = cmp.mapping.preset.insert({
-          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = Util.cmp.confirm(),
+          ["<CR>"] = Util.cmp.confirm({ select = auto_select }),
+          ["<C-y>"] = Util.cmp.confirm({ select = true }),
           ["<S-CR>"] = Util.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
           ["<C-CR>"] = function(fallback)
             cmp.abort()
@@ -48,11 +48,23 @@ return {
           { name = "buffer" },
         }),
         formatting = {
-          format = function(_, item)
-            local icons = require("wrightbradley.config").icons.kinds
+          format = function(entry, item)
+            local icons = Util.config.icons.kinds
             if icons[item.kind] then
               item.kind = icons[item.kind] .. item.kind
             end
+
+            local widths = {
+              abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 40,
+              menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 30,
+            }
+
+            for key, width in pairs(widths) do
+              if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
+                item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. "â¦"
+              end
+            end
+
             return item
           end,
         },
@@ -64,74 +76,49 @@ return {
         sorting = defaults.sorting,
       }
     end,
-    ---@param opts cmp.ConfigSchema | {auto_brackets?: string[]}
-    config = function(_, opts)
-      for _, source in ipairs(opts.sources) do
-        source.group_index = source.group_index or 1
-      end
-
-      local parse = require("cmp.utils.snippet").parse
-      require("cmp.utils.snippet").parse = function(input)
-        local ok, ret = pcall(parse, input)
-        if ok then
-          return ret
-        end
-        return Util.cmp.snippet_preview(input)
-      end
-
-      local cmp = require("cmp")
-      cmp.setup(opts)
-      cmp.event:on("confirm_done", function(event)
-        if vim.tbl_contains(opts.auto_brackets or {}, vim.bo.filetype) then
-          Util.cmp.auto_brackets(event.entry)
-        end
-      end)
-      cmp.event:on("menu_opened", function(event)
-        Util.cmp.add_missing_snippet_docs(event.window)
-      end)
-    end,
+    main = "wrightbradley.util.cmp",
   },
-
   -- snippets
-  vim.fn.has("nvim-0.10") == 1
-    and {
-      "nvim-cmp",
-      dependencies = {
-        {
-          "garymjr/nvim-snippets",
-          opts = {
-            friendly_snippets = true,
-          },
-          dependencies = { "rafamadriz/friendly-snippets" },
+  {
+    "nvim-cmp",
+    dependencies = {
+      {
+        "garymjr/nvim-snippets",
+        opts = {
+          friendly_snippets = true,
         },
-      },
-      opts = function(_, opts)
-        opts.snippet = {
-          expand = function(item)
-            return Util.cmp.expand(item.body)
-          end,
-        }
-        table.insert(opts.sources, { name = "snippets" })
-      end,
-      keys = {
-        {
-          "<Tab>",
-          function()
-            return vim.snippet.active({ direction = 1 }) and "<cmd>lua vim.snippet.jump(1)<cr>" or "<Tab>"
-          end,
-          expr = true,
-          silent = true,
-          mode = { "i", "s" },
-        },
-        {
-          "<S-Tab>",
-          function()
-            return vim.snippet.active({ direction = -1 }) and "<cmd>lua vim.snippet.jump(-1)<cr>" or "<Tab>"
-          end,
-          expr = true,
-          silent = true,
-          mode = { "i", "s" },
-        },
+        dependencies = { "rafamadriz/friendly-snippets" },
       },
     },
+    opts = function(_, opts)
+      opts.snippet = {
+        expand = function(item)
+          return Util.cmp.expand(item.body)
+        end,
+      }
+      if Util.has("nvim-snippets") then
+        table.insert(opts.sources, { name = "snippets" })
+      end
+    end,
+    keys = {
+      {
+        "<Tab>",
+        function()
+          return vim.snippet.active({ direction = 1 }) and "<cmd>lua vim.snippet.jump(1)<cr>" or "<Tab>"
+        end,
+        expr = true,
+        silent = true,
+        mode = { "i", "s" },
+      },
+      {
+        "<S-Tab>",
+        function()
+          return vim.snippet.active({ direction = -1 }) and "<cmd>lua vim.snippet.jump(-1)<cr>" or "<S-Tab>"
+        end,
+        expr = true,
+        silent = true,
+        mode = { "i", "s" },
+      },
+    },
+  },
 }
