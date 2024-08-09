3d67891416b218ba92ffbf1b4651f100ae64a7e7
wrightbradley <bradley.wright.tech@gmail.com>
2024-08-09 09:02:12 -0400

    feat: update plugins/json
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/json.lua
+++ lua/wrightbradley/plugins/json.lua
@@ -2,11 +2,7 @@ return {
   -- add json to treesitter
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "json", "json5", "jsonc" })
-      end
-    end,
+    opts = { ensure_installed = { "json5" } },
   },
 
   -- yaml schema support
