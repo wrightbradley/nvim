e28516637fd5bd5d8c9a60b86679cc344abdf398
wrightbradley <bradley.wright.tech@gmail.com>
2024-08-09 08:00:33 -0400

    remove: unused plugins
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/ansible.lua
+++ lua/wrightbradley/plugins/ansible.lua
@@ -41,18 +41,4 @@ return {
       },
     },
   },
-  {
-    "mfussenegger/nvim-ansible",
-    ft = {},
-    keys = {
-      {
-        "<leader>ta",
-        function()
-          require("ansible").run()
-        end,
-        desc = "Ansible Run Playbook/Role",
-        silent = true,
-      },
-    },
-  },
 }
--- lua/wrightbradley/plugins/native-snippets.lua
+++ lua/wrightbradley/plugins/native-snippets.lua
@@ -1,6 +1,3 @@
-if true then
-  return {}
-end
 return {
   desc = "Use native snippets instead of LuaSnip. Only works on Neovim >= 0.10!",
   {
