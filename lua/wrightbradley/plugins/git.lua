25c7f276c3b5fe6df5242e9ed7cc1b2a8389bf44
wrightbradley <bradley.wright.tech@gmail.com>
2024-08-09 09:13:42 -0400

    feat: update plugins/git
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/git.lua
+++ lua/wrightbradley/plugins/git.lua
@@ -1,13 +1,20 @@
 return {
+  -- Treesitter git support
   {
     "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "git_config", "gitcommit", "git_rebase", "gitignore", "gitattributes" } },
+  },
+
+  {
+    "nvim-cmp",
+    dependencies = {
+      { "petertriho/cmp-git", opts = {} },
+    },
+    ---@module 'cmp'
+    ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(
-          opts.ensure_installed,
-          { "git_config", "git_rebase", "gitattributes", "gitcommit", "gitignore", "octo" }
-        )
-      end
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, { name = "git" })
     end,
   },
   {
