0c7dafc9254a25fd8532f6ec8f78640485dd54de
wrightbradley <bradley.wright.tech@gmail.com>
2024-08-19 17:04:53 -0400

    style: set neovim to be transparent
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/colorscheme/init.lua
+++ lua/wrightbradley/plugins/colorscheme/init.lua
@@ -3,7 +3,14 @@ return {
     "folke/tokyonight.nvim",
     lazy = true,
     priority = 1000,
-    opts = { style = "night" },
+    opts = {
+      style = "night",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
     config = function(_, opts)
       require("tokyonight").setup(opts)
       vim.cmd([[colorscheme tokyonight]])
