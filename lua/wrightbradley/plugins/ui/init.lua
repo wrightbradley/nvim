276ee475b6d5335d0e0f3411b86674c993617223
wrightbradley <bradley.wright.tech@gmail.com>
2024-08-27 10:26:20 -0400

    feat: add linters to lualine
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/ui/init.lua
+++ lua/wrightbradley/plugins/ui/init.lua
@@ -213,11 +213,17 @@ return {
             { "progress", separator = " ", padding = { left = 1, right = 1 } },
             -- { "location", padding = { left = 0, right = 1 } },
           },
-          -- lualine_z = {
-          --   function()
-          --     return " " .. os.date("%R")
-          --   end,
-          -- },
+          lualine_z = {
+            function()
+              -- https://github.com/mfussenegger/nvim-lint/issues/583
+              -- local linters = require("lint").get_running()
+              local linters = require("lint").linters_by_ft[vim.bo.filetype] or {}
+              if #linters == 0 then
+                return "󰦕"
+              end
+              return "󱉶 " .. table.concat(linters, ", ")
+            end,
+          },
         },
         extensions = { "neo-tree", "lazy" },
       }
