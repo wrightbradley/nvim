9b9ee646e44421fb8ced2221bb95ce1cbfc4658d
wrightbradley <bradley.wright.tech@gmail.com>
2024-10-03 08:12:59 -0400

    fix: support new ts_ls server name
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/lang/typescript.lua
+++ lua/wrightbradley/plugins/lang/typescript.lua
@@ -5,7 +5,12 @@ return {
     opts = {
       -- make sure mason installs the server
       servers = {
+        --- @deprecated -- tsserver renamed to ts_ls but not yet released, so keep this for now
+        --- the proper approach is to check the nvim-lspconfig release version when it's released to determine the server name dynamically
         tsserver = {
+          enabled = false,
+        },
+        ts_ls = {
           enabled = false,
         },
         vtsls = {
@@ -100,7 +105,13 @@ return {
         },
       },
       setup = {
+        --- @deprecated -- tsserver renamed to ts_ls but not yet released, so keep this for now
+        --- the proper approach is to check the nvim-lspconfig release version when it's released to determine the server name dynamically
         tsserver = function()
+          -- disable tsserver
+          return true
+        end,
+        ts_ls = function()
           -- disable tsserver
           return true
         end,
