0a99828f175b7b196bcea10a1432ff06dd43a474
wrightbradley <bradley.wright.tech@gmail.com>
2024-11-19 06:34:47 -0500

    fix(lualine): dynamically fetch the color used in lualine
    The color used in lualine was previously static and did not update
    dynamically when a new colorscheme was applied. This resulted in
    inconsistencies between the theme and the lualine colors, as the 'local'
    color table returned the highlight color defined during initialization.
    
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/util/lualine.lua
+++ lua/wrightbradley/util/lualine.lua
@@ -1,13 +1,37 @@
 ---@class wrightbradley.util.lualine
 local M = {}
 
+---@param icon string
+---@param status fun(): nil|"ok"|"error"|"pending"
+function M.status(icon, status)
+  local colors = {
+    ok = "Special",
+    error = "DiagnosticError",
+    pending = "DiagnosticWarn",
+  }
+  return {
+    function()
+      return icon
+    end,
+    cond = function()
+      return status() ~= nil
+    end,
+    color = function()
+      return Util.ui.fg(colors[status()] or colors.ok)
+    end,
+  }
+end
+
+---@param name string
+---@param icon? string
 function M.cmp_source(name, icon)
+  icon = icon or Util.config.icons.kinds[name:sub(1, 1):upper() .. name:sub(2)]
   local started = false
-  local function status()
+  return M.status(icon, function()
     if not package.loaded["cmp"] then
       return
     end
-    for _, s in ipairs(require("cmp").core.sources) do
+    for _, s in ipairs(require("cmp").core.sources or {}) do
       if s.name == name then
         if s.source:is_available() then
           started = true
@@ -20,25 +44,7 @@ function M.cmp_source(name, icon)
         return "ok"
       end
     end
-  end
-
-  local colors = {
-    ok = Util.ui.fg("Special"),
-    error = Util.ui.fg("DiagnosticError"),
-    pending = Util.ui.fg("DiagnosticWarn"),
-  }
-
-  return {
-    function()
-      return icon or Util.config.icons.kinds[name:sub(1, 1):upper() .. name:sub(2)]
-    end,
-    cond = function()
-      return status() ~= nil
-    end,
-    color = function()
-      return colors[status()] or colors.ok
-    end,
-  }
+  end)
 end
 
 ---@param component any
@@ -91,6 +97,7 @@ function M.pretty_path(opts)
       return ""
     end
 
+    path = Util.norm(path)
     local root = Util.root.get({ normalize = true })
     local cwd = Util.root.cwd()
 
@@ -138,7 +145,9 @@ function M.root_dir(opts)
     parent = true,
     other = true,
     icon = "󱉭 ",
-    color = Util.ui.fg("Special"),
+    color = function()
+      return Util.ui.fg("Special")
+    end,
   }, opts or {})
 
   local function get()
