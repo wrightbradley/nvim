590c8aa8b96700c19a91cada1f9f42bf01d3ac75
wrightbradley <bradley.wright.tech@gmail.com>
2024-12-02 08:37:38 -0500

    feat: configure ai tools
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/utilities/codeium.lua
+++ lua/wrightbradley/plugins/utilities/codeium.lua
@@ -1,24 +1,46 @@
-if true then
+local codeium_enabled = os.getenv("NVIM_ENABLE_CODEIUM")
+if codeium_enabled == "false" then
   return {}
 end
 return {
+  -- codeium
+  {
+    "Exafunction/codeium.nvim",
+    cmd = "Codeium",
+    build = ":Codeium Auth",
+    opts = {
+      enable_cmp_source = vim.g.ai_cmp,
+      virtual_text = {
+        enabled = not vim.g.ai_cmp,
+        key_bindings = {
+          accept = false, -- handled by nvim-cmp / blink.cmp
+          next = "<M-]>",
+          prev = "<M-[>",
+        },
+      },
+    },
+  },
+
+  -- add ai_accept action
+  {
+    "Exafunction/codeium.nvim",
+    opts = function()
+      Util.cmp.actions.ai_accept = function()
+        if require("codeium.virtual_text").get_current_completion_item() then
+          Util.create_undo()
+          vim.api.nvim_input(require("codeium.virtual_text").accept())
+          return true
+        end
+      end
+    end,
+  },
+
   -- codeium cmp source
   {
     "nvim-cmp",
-    dependencies = {
-      -- codeium
-      {
-        "Exafunction/codeium.nvim",
-        cmd = "Codeium",
-        build = ":Codeium Auth",
-        opts = {},
-      },
-    },
-    ---@param opts cmp.ConfigSchema
+    optional = true,
+    dependencies = { "codeium.nvim" },
     opts = function(_, opts)
-      -- vim.api.nvim_out_write("This message will be printed to the message area.\n")
-      -- vim.api.nvim_out_write(opts)
-      -- opts.sources = opts.sources or {}
       table.insert(opts.sources, 1, {
         name = "codeium",
         group_index = 1,
@@ -34,5 +56,19 @@ return {
     opts = function(_, opts)
       table.insert(opts.sections.lualine_x, 2, Util.lualine.cmp_source("codeium"))
     end,
+  },
+
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    opts = {
+      sources = {
+        compat = vim.g.ai_cmp and { "codeium" } or nil,
+      },
+    },
+    dependencies = {
+      "codeium.nvim",
+      vim.g.ai_cmp and "saghen/blink.compat" or nil,
+    },
   },
 }
