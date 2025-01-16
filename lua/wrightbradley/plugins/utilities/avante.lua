12bd7eb40df84f2a3afad9d6b0faf6126736a43a
wrightbradley <bradley.wright.tech@gmail.com>
2025-01-16 10:12:26 -0500

    feat: disable avante hints
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/utilities/avante.lua
+++ lua/wrightbradley/plugins/utilities/avante.lua
@@ -33,6 +33,7 @@ return {
             },
             -- required for Windows users
             use_absolute_path = true,
+            hints = { enabled = false },
           },
         },
       },
