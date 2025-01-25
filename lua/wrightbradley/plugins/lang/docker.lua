f56c4b88321ad70fad980e1f83df91f50fe5f086
wrightbradley <bradley.wright.tech@gmail.com>
2025-01-25 13:10:58 -0500

    feat: update lang/docker
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/lang/docker.lua
+++ lua/wrightbradley/plugins/lang/docker.lua
@@ -1,17 +1,20 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "dockerfile" })
-      end
-    end,
+    opts = { ensure_installed = { "dockerfile" } },
   },
   {
     "mason.nvim",
+    opts = { ensure_installed = { "hadolint" } },
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
     opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "hadolint", "dockerfile-language-server" })
+      local nls = require("null-ls")
+      opts.sources = vim.list_extend(opts.sources or {}, {
+        nls.builtins.diagnostics.hadolint,
+      })
     end,
   },
   {
