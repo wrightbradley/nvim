ae4047b32673a7fbaea8c849ba7c6d2c85ec6d98
wrightbradley <bradley.wright.tech@gmail.com>
2025-01-25 13:15:09 -0500

    feat: update lang/toml
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/lang/toml.lua
+++ lua/wrightbradley/plugins/lang/toml.lua
@@ -1,37 +1,8 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "toml" })
-      end
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "taplo" })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        taplo = {},
-      },
-    },
-  },
-  -- Filetype icons
-  {
-    "echasnovski/mini.icons",
-    opts = {
-      file = {
-        ["toml.tmpl"] = { glyph = "", hl = "MiniIconsGrey" },
-      },
-      filetype = {
-        toml = { glyph = "", hl = "MiniIconsGrey" },
-      },
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      taplo = {},
     },
   },
 }
