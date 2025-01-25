8ac066b2da8ea81695b2bef7e1ea9a8ec646ce64
wrightbradley <bradley.wright.tech@gmail.com>
2025-01-25 13:10:44 -0500

    feat: update lang/cmake
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/lang/cmake.lua
+++ lua/wrightbradley/plugins/lang/cmake.lua
@@ -1,10 +1,16 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "cmake" } },
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
     opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "cmake" })
-      end
+      local nls = require("null-ls")
+      opts.sources = vim.list_extend(opts.sources or {}, {
+        nls.builtins.diagnostics.cmake_lint,
+      })
     end,
   },
   {
@@ -18,10 +24,7 @@ return {
   },
   {
     "mason.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "cmakelang", "cmakelint" })
-    end,
+    opts = { ensure_installed = { "cmakelang", "cmakelint" } },
   },
   {
     "neovim/nvim-lspconfig",
