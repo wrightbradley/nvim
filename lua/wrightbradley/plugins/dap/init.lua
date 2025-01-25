56519d7da4ac204361af92ac2fc0fe5ca130e71e
wrightbradley <bradley.wright.tech@gmail.com>
2025-01-25 11:01:19 -0500

    feat: update plugins/dap
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/dap/init.lua
+++ lua/wrightbradley/plugins/dap/init.lua
@@ -31,47 +31,46 @@ return {
       },
       {
         "jbyuki/one-small-step-for-vimkind",
-        -- stylua: ignore
-        config = function()
-          local dap = require("dap")
-          dap.adapters.nlua = function(callback, conf)
-            local adapter = {
-              type = "server",
-              host = conf.host or "127.0.0.1",
-              port = conf.port or 8086,
-            }
-            if conf.start_neovim then
-              local dap_run = dap.run
-              dap.run = function(c)
-                adapter.port = c.port
-                adapter.host = c.host
-              end
-              require("osv").run_this()
-              dap.run = dap_run
-            end
-            callback(adapter)
-          end
-          dap.configurations.lua = {
-            {
-              type = "nlua",
-              request = "attach",
-              name = "Run this file",
-              start_neovim = {},
-            },
-            {
-              type = "nlua",
-              request = "attach",
-              name = "Attach to running Neovim instance (port = 8086)",
-              port = 8086,
-            },
+      -- stylua: ignore
+      config = function()
+        local dap = require("dap")
+        dap.adapters.nlua = function(callback, conf)
+          local adapter = {
+            type = "server",
+            host = conf.host or "127.0.0.1",
+            port = conf.port or 8086,
           }
-        end,
+          if conf.start_neovim then
+            local dap_run = dap.run
+            dap.run = function(c)
+              adapter.port = c.port
+              adapter.host = c.host
+            end
+            require("osv").run_this()
+            dap.run = dap_run
+          end
+          callback(adapter)
+        end
+        dap.configurations.lua = {
+          {
+            type = "nlua",
+            request = "attach",
+            name = "Run this file",
+            start_neovim = {},
+          },
+          {
+            type = "nlua",
+            request = "attach",
+            name = "Attach to running Neovim instance (port = 8086)",
+            port = 8086,
+          },
+        }
+      end,
       },
     },
 
     -- stylua: ignore
     keys = {
-      { "<leader>d", "", desc = "+debug", mode = {"n", "v"} },
       { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
       { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
       { "<leader>dc", function() require("dap").continue() end, desc = "Run/Continue" },
@@ -84,7 +83,7 @@ return {
       { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
       { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
       { "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
-      { "<leader>dp", function() require("dap").pause() end, desc = "Pause" },
+      { "<leader>dP", function() require("dap").pause() end, desc = "Pause" },
       { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
       { "<leader>ds", function() require("dap").session() end, desc = "Session" },
       { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
