5cfcc55c7cfec40754ff16c63e4cbfc652040126
wrightbradley <bradley.wright.tech@gmail.com>
2025-01-25 13:10:32 -0500

    feat: update lang/clangd
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/lang/clangd.lua
+++ lua/wrightbradley/plugins/lang/clangd.lua
@@ -88,12 +88,15 @@ return {
     },
   },
 
-  -- {
-  --   "nvim-cmp",
-  --   opts = function(_, opts)
-  --     table.insert(opts.sorting.comparators, 1, require("clangd_extensions.cmp_scores"))
-  --   end,
-  -- },
+  {
+    "hrsh7th/nvim-cmp",
+    optional = true,
+    opts = function(_, opts)
+      opts.sorting = opts.sorting or {}
+      opts.sorting.comparators = opts.sorting.comparators or {}
+      table.insert(opts.sorting.comparators, 1, require("clangd_extensions.cmp_scores"))
+    end,
+  },
 
   {
     "mfussenegger/nvim-dap",
