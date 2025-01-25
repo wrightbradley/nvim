5648f1342ffa1e4aa6df78306d9bd54f9568d4b2
wrightbradley <bradley.wright.tech@gmail.com>
2025-01-25 13:13:08 -0500

    feat: update lang/nix
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/lang/nix.lua
+++ lua/wrightbradley/plugins/lang/nix.lua
@@ -4,28 +4,23 @@ end
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "nix" })
-      end
-    end,
+    opts = { ensure_installed = { "nix" } },
   },
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        nixd = {},
+        nil_ls = {},
       },
     },
   },
   {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "nixd",
-        "nil",
-        "nixpkgs-fmt",
-      })
-    end,
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        nix = { "nixfmt" },
+      },
+    },
   },
 }
