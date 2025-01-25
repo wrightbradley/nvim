c5b44331e1617c23e4545d4c8bbab736fee668aa
wrightbradley <bradley.wright.tech@gmail.com>
2025-01-25 13:14:28 -0500

    feat: update lang/ruby
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/lang/ruby.lua
+++ lua/wrightbradley/plugins/lang/ruby.lua
@@ -1,27 +1,52 @@
+if true then
+  return {}
+end
+if wrightbradley_docs then
+  -- LSP Server to use for Ruby.
+  -- Set to "solargraph" to use solargraph instead of ruby_lsp.
+  vim.g.wrightbradley_ruby_lsp = "ruby_lsp"
+  vim.g.wrightbradley_ruby_formatter = "rubocop"
+end
+
+local lsp = vim.g.wrightbradley_ruby_lsp or "ruby_lsp"
+if vim.fn.has("nvim-0.10") == 0 then
+  -- ruby_lsp does not work well with Neovim < 0.10
+  lsp = vim.g.wrightbradley_ruby_lsp or "solargraph"
+end
+local formatter = vim.g.wrightbradley_ruby_formatter or "rubocop"
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "ruby",
-      })
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        -- "solargraph",
-      })
-    end,
+    opts = { ensure_installed = { "ruby" } },
   },
   {
     "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
     opts = {
+      ---@type lspconfig.options
       servers = {
-        -- solargraph = {},
+        ruby_lsp = {
+          enabled = lsp == "ruby_lsp",
+        },
+        solargraph = {
+          enabled = lsp == "solargraph",
+        },
+        rubocop = {
+          -- If Solargraph and Rubocop are both enabled as an LSP,
+          -- diagnostics will be duplicated because Solargraph
+          -- already calls Rubocop if it is installed
+          enabled = formatter == "rubocop" and lsp ~= "solargraph",
+        },
+        standardrb = {
+          enabled = formatter == "standardrb",
+        },
       },
     },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = { ensure_installed = { "erb-formatter", "erb-lint" } },
   },
   {
     "mfussenegger/nvim-dap",
@@ -31,6 +56,16 @@ return {
       config = function()
         require("dap-ruby").setup()
       end,
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ruby = { formatter },
+        eruby = { "erb_format" },
+      },
     },
   },
   {
