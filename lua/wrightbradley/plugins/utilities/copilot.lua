c44cae722a6f781e994673fcf08cffa5a2fc600c
wrightbradley <bradley.wright.tech@gmail.com>
2025-01-25 13:16:44 -0500

    feat: update utilities/copilot
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/utilities/copilot.lua
+++ lua/wrightbradley/plugins/utilities/copilot.lua
@@ -1,4 +1,5 @@
-if true then
+local copilot_enabled = os.getenv("NVIM_ENABLE_COPILOT")
+if copilot_enabled == "false" then
   return {}
 end
 -- copilot suggestions and completion
@@ -8,8 +9,17 @@ return {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
     build = ":Copilot auth",
+    event = "InsertEnter",
     opts = {
-      suggestion = { enabled = false },
+      suggestion = {
+        enabled = not vim.g.ai_cmp,
+        auto_trigger = true,
+        keymap = {
+          accept = false, -- handled by nvim-cmp / blink.cmp
+          next = "<M-]>",
+          prev = "<M-[>",
+        },
+      },
       panel = { enabled = false },
       filetypes = {
         markdown = true,
@@ -17,161 +27,96 @@ return {
       },
     },
   },
+
+  -- add ai_accept action
+  {
+    "zbirenbaum/copilot.lua",
+    opts = function()
+      Util.cmp.actions.ai_accept = function()
+        if require("copilot.suggestion").is_visible() then
+          Util.create_undo()
+          require("copilot.suggestion").accept()
+          return true
+        end
+      end
+    end,
+  },
+
+  -- lualine
   {
     "nvim-lualine/lualine.nvim",
     optional = true,
     event = "VeryLazy",
     opts = function(_, opts)
-      local colors = {
-        [""] = Util.ui.fg("Special"),
-        ["Normal"] = Util.ui.fg("Special"),
-        ["Warning"] = Util.ui.fg("DiagnosticError"),
-        ["InProgress"] = Util.ui.fg("DiagnosticWarn"),
+      table.insert(
+        opts.sections.lualine_x,
+        2,
+        Util.lualine.status(Util.config.icons.kinds.Copilot, function()
+          local clients = package.loaded["copilot"] and Util.lsp.get_clients({ name = "copilot", bufnr = 0 }) or {}
+          if #clients > 0 then
+            local status = require("copilot.api").status.data.status
+            return (status == "InProgress" and "pending") or (status == "Warning" and "error") or "ok"
+          end
+        end)
+      )
+    end,
+  },
+
+  vim.g.ai_cmp
+      and {
+        -- copilot cmp source
+        {
+          "hrsh7th/nvim-cmp",
+          optional = true,
+          dependencies = { -- this will only be evaluated if nvim-cmp is enabled
+            {
+              "zbirenbaum/copilot-cmp",
+              opts = {},
+              config = function(_, opts)
+                local copilot_cmp = require("copilot_cmp")
+                copilot_cmp.setup(opts)
+                -- attach cmp source whenever copilot attaches
+                -- fixes lazy-loading issues with the copilot cmp source
+                Util.lsp.on_attach(function()
+                  copilot_cmp._on_insert_enter({})
+                end, "copilot")
+              end,
+              specs = {
+                {
+                  "hrsh7th/nvim-cmp",
+                  optional = true,
+                  ---@param opts cmp.ConfigSchema
+                  opts = function(_, opts)
+                    table.insert(opts.sources, 1, {
+                      name = "copilot",
+                      group_index = 1,
+                      priority = 100,
+                    })
+                  end,
+                },
+              },
+            },
+          },
+        },
+        {
+          "saghen/blink.cmp",
+          optional = true,
+          dependencies = { "giuxtaposition/blink-cmp-copilot" },
+          opts = {
+            sources = {
+              default = { "copilot" },
+              providers = {
+                copilot = {
+                  name = "copilot",
+                  module = "blink-cmp-copilot",
+                  kind = "Copilot",
+                  score_offset = 100,
+                  async = true,
+                },
+              },
+            },
+          },
+        },
       }
-      table.insert(opts.sections.lualine_x, 2, {
-        function()
-          local icon = require("wrightbradley.config").icons.kinds.Copilot
-          local status = require("copilot.api").status.data
-          return icon .. (status.message or "")
-        end,
-        cond = function()
-          if not package.loaded["copilot"] then
-            return
-          end
-          local ok, clients = pcall(Util.lsp.get_clients, { name = "copilot", bufnr = 0 })
-          if not ok then
-            return false
-          end
-          return ok and #clients > 0
-        end,
-        color = function()
-          if not package.loaded["copilot"] then
-            return
-          end
-          local status = require("copilot.api").status.data
-          return colors[status.status] or colors[""]
-        end,
-      })
-    end,
-  },
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
-    dependencies = {
-      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-    },
-    keys = {
-      -- Code related commands
-      { "<leader>cce", "<Cmd>CopilotChatExplain<CR>", desc = "CopilotChat - Explain code" },
-      { "<leader>cct", "<Cmd>CopilotChatTests<CR>", desc = "CopilotChat - Generate tests" },
-      { "<leader>ccr", "<Cmd>CopilotChatReview<CR>", desc = "CopilotChat - Review code" },
-      { "<leader>ccR", "<Cmd>CopilotChatRefactor<CR>", desc = "CopilotChat - Refactor code" },
-      { "<leader>ccn", "<Cmd>CopilotChatBetterNamings<CR>", desc = "CopilotChat - Better Naming" },
-      -- Chat with Copilot in visual mode
-      { "<leader>ccv", ":CopilotChatVisual ", mode = "x", desc = "CopilotChat - Chat with vertical split" },
-      { "<leader>ccx", ":CopilotChatInline<CR>", mode = "x", desc = "CopilotChat - Inline chat" },
-      -- Custom input for CopilotChat
-      {
-        "<leader>cci",
-        function()
-          local input = vim.fn.input("Ask Copilot: ")
-          if input ~= "" then
-            vim.cmd("CopilotChat " .. input)
-          end
-        end,
-        desc = "CopilotChat - Ask input",
-      },
-      -- Generate commit message based on the git diff
-      { "<leader>ccm", "<Cmd>CopilotChatCommit<CR>", desc = "CopilotChat - Generate commit message for all changes" },
-      {
-        "<leader>ccM",
-        "<Cmd>CopilotChatCommitStaged<CR>",
-        desc = "CopilotChat - Generate commit message for staged changes",
-      },
-      -- Quick chat with Copilot
-      {
-        "<leader>ccq",
-        function()
-          local input = vim.fn.input("Quick Chat: ")
-          if input ~= "" then
-            vim.cmd("CopilotChatBuffer " .. input)
-          end
-        end,
-        desc = "CopilotChat - Quick chat",
-      },
-      -- Debug
-      { "<leader>ccd", "<Cmd>CopilotChatDebugInfo<CR>", desc = "CopilotChat - Debug Info" },
-      -- Fix the issue with diagnostic
-      { "<leader>ccf", "<Cmd>CopilotChatFixDiagnostic<CR>", desc = "CopilotChat - Fix Diagnostic" },
-      -- Clear buffer and chat history
-      { "<leader>ccl", "<Cmd>CopilotChatReset<CR>", desc = "CopilotChat - Clear buffer and chat history" },
-      --  -- Toggle Copilot Chat Vsplit
-      --  { "<leader>ccv", "<Cmd>CopilotChatToggle<CR>", desc = "CopilotChat - Toggle Vsplit" },
-      -- Show help actions with telescope
-      {
-        "<leader>cch",
-        function()
-          local actions = require("CopilotChat.actions")
-          require("CopilotChat.integrations.telescope").pick(actions.help_actions())
-        end,
-        desc = "CopilotChat - Help actions",
-      }, -- show help actions with telescope
-      {
-        "<leader>ccp",
-        function()
-          local actions = require("CopilotChat.actions")
-          require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
-        end,
-        desc = "CopilotChat - Help actions",
-      }, -- show prompts actions with telescope
-      {
-        "<leader>ccp",
-        ":lua require('CopilotChat.integrations.telescope').pick(require('CopilotChat.actions').prompt_actions())<CR>",
-        desc = "CopilotChat - Prompt actions",
-      },
-    },
-    opts = {
-      debug = true, -- Enable debugging
-      window = {
-        layout = "float",
-        relative = "cursor",
-        width = 1,
-        height = 0.4,
-        row = 1,
-      },
-      -- See Configuration section for rest
-    },
-    -- See Commands section for default commands if you want to lazy load on them
-  },
-  -- copilot cmp source
-  {
-    "nvim-cmp",
-    dependencies = {
-      {
-        "zbirenbaum/copilot-cmp",
-        dependencies = "copilot.lua",
-        opts = {},
-        config = function(_, opts)
-          local copilot_cmp = require("copilot_cmp")
-          copilot_cmp.setup(opts)
-          -- attach cmp source whenever copilot attaches
-          -- fixes lazy-loading issues with the copilot cmp source
-          Util.lsp.on_attach(function(client)
-            if client.name == "copilot" then
-              copilot_cmp._on_insert_enter({})
-            end
-          end)
-        end,
-      },
-    },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      table.insert(opts.sources, 1, {
-        name = "copilot",
-        group_index = 1,
-        priority = 100,
-      })
-    end,
-  },
+    or nil,
 }
