022e679a113ea9f3d9268a3edad215128f9b8a68
wrightbradley <bradley.wright.tech@gmail.com>
2025-04-07 10:07:32 -0400

    style: use rounded borders
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/config/options.lua
+++ lua/wrightbradley/config/options.lua
@@ -127,6 +127,7 @@ opt.updatetime = 200 -- Save swap file and trigger CursorHold
 opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
 opt.wildmode = "longest:full,full" -- Command-line completion mode
 opt.winminwidth = 5 -- Minimum window width
+opt.winborder = "rounded"
 opt.wrap = false -- Disable line wrap
 
 if vim.fn.has("nvim-0.10") == 1 then
