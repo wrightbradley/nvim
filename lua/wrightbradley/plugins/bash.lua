d2c495031af3725c5d80e51109c7f06f66efccc1
wrightbradley <bradley.wright.tech@gmail.com>
2025-05-29 17:22:55 -0400

    chore: update bash config
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/bash.lua
+++ lua/wrightbradley/plugins/bash.lua
@@ -1,11 +1,7 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "bash" })
-      end
-    end,
+    opts = { ensure_installed = { "bash" } },
   },
   {
     "neovim/nvim-lspconfig",
@@ -17,13 +13,7 @@ return {
   },
   {
     "mason-org/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "bash-language-server",
-        "shfmt",
-        "shellcheck",
-      })
-    end,
+    opts = { ensure_installed = { "bash-language-server", "shfmt", "shellcheck" } },
   },
   {
     "mfussenegger/nvim-lint",
