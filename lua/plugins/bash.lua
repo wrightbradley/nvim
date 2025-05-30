8c73b199ccf1ba87ae36f1c9d6387203829fcec0
wrightbradley <bradley.wright.tech@gmail.com>
2025-05-30 14:54:50 -0400

    refactor: migrate to new lsp handling for bash
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/plugins/bash.lua
+++ lua/plugins/bash.lua
@@ -1,13 +1,5 @@
 return {
   {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        bashls = {},
-      },
-    },
-  },
-  {
     "mason-org/mason.nvim",
     opts = { ensure_installed = { "bash-language-server", "shfmt", "shellcheck" } },
   },
