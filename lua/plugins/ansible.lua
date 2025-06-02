ffd7f13cf76aa70b74e43c4620d42304e44240c5
wrightbradley <bradley.wright.tech@gmail.com>
2025-06-02 09:40:27 -0400

    refactor: migrate to new lsp handling for ansible
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

---
+++ lua/plugins/ansible.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "mfussenegger/nvim-ansible",
+    ft = {},
+    -- keys = {
+    --   {
+    --     "<leader>ta",
+    --     function()
+    --       require("plugins.ansible").run()
+    --     end,
+    --     desc = "Ansible Run Playbook/Role",
+    --     silent = true,
+    --   },
+    -- },
+  },
+}
