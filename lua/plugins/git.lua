1666633c389e951648184cf82e8ea9089dff8c66
wrightbradley <bradley.wright.tech@gmail.com>
2025-06-04 12:56:18 -0400

    chore: switch git worktree plugin
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -64,72 +64,71 @@ return {
       { "#", "#<C-x><C-o>", mode = "i", ft = "octo", silent = true },
     },
   },
-  {
-    "polarmutex/git-worktree.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-    config = function()
-      local Hooks = require("git-worktree.hooks")
-      require("telescope").load_extension("git_worktree")
-      Hooks.register(Hooks.type.SWITCH, function(path, prev_path)
-        local relativePath = path:gsub("^" .. os.getenv("HOME"), "")
-        vim.notify("Switched to ~" .. relativePath)
-
-        -- Update the current buffer
-        Hooks.builtins.update_current_buffer_on_switch(path, prev_path)
-
-        for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-          if vim.api.nvim_buf_is_loaded(buf) then
-            vim.api.nvim_buf_delete(buf, { force = true })
-          end
-        end
-      end)
-    end,
-    keys = {
-      {
-        "<leader>gws",
-        function()
-          require("telescope").extensions.git_worktree.git_worktree()
-        end,
-        desc = "Git Worktree switch",
-      },
-      {
-        "<leader>gwc",
-        function()
-          require("telescope").extensions.git_worktree.create_git_worktree()
-        end,
-        desc = "Create new Git Worktree",
-      },
-    },
-  },
   -- {
-  --   "Juksuu/worktrees.nvim",
+  --   "polarmutex/git-worktree.nvim",
   --   dependencies = {
   --     "nvim-lua/plenary.nvim",
   --     "nvim-telescope/telescope.nvim",
   --   },
   --   config = function()
-  --     require("worktrees").setup()
+  --     local Hooks = require("git-worktree.hooks")
+  --     require("telescope").load_extension("git_worktree")
+  --     Hooks.register(Hooks.type.SWITCH, function(path, prev_path)
+  --       local relativePath = path:gsub("^" .. os.getenv("HOME"), "")
+  --       vim.notify("Switched to ~" .. relativePath)
+  --
+  --       -- Update the current buffer
+  --       Hooks.builtins.update_current_buffer_on_switch(path, prev_path)
+  --
+  --       for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+  --         if vim.api.nvim_buf_is_loaded(buf) then
+  --           vim.api.nvim_buf_delete(buf, { force = true })
+  --         end
+  --       end
+  --     end)
   --   end,
   --   keys = {
   --     {
   --       "<leader>gws",
   --       function()
-  --         Snacks.picker.worktrees()
+  --         require("telescope").extensions.git_worktree.git_worktree()
   --       end,
   --       desc = "Git Worktree switch",
   --     },
   --     {
   --       "<leader>gwc",
   --       function()
-  --         require("worktrees").new_worktree()
+  --         require("telescope").extensions.git_worktree.create_git_worktree()
   --       end,
   --       desc = "Create new Git Worktree",
   --     },
   --   },
   -- },
+  {
+    "Juksuu/worktrees.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("worktrees").setup()
+    end,
+    keys = {
+      {
+        "<leader>gws",
+        function()
+          Snacks.picker.worktrees()
+        end,
+        desc = "Git Worktree switch",
+      },
+      {
+        "<leader>gwn",
+        function()
+          Snacks.picker.worktrees_new()
+        end,
+        desc = "Create new Git Worktree",
+      },
+    },
+  },
   {
     "dlvhdr/gh-addressed.nvim",
     dependencies = {
