08390a5a41d79534526679bc6a749de7ee873671
wrightbradley <bradley.wright.tech@gmail.com>
2025-12-17 12:56:31 -0500

    refactor: migrate todo-comments from Telescope to Snacks picker
    - Replace TodoTelescope commands with Snacks.picker.pick("todo")
    - Update cmd list to use TodoQuickFix and TodoLocList instead of TodoTelescope
    - Maintain same keybindings (<leader>st and <leader>sT) with Snacks picker backend
    
    This reduces dependency on Telescope (now only optional for Terraform) and unifies the picker experience around Snacks.

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -3,7 +3,7 @@ return {
   -- in your project and loads them into a browsable list.
   {
     "folke/todo-comments.nvim",
-    cmd = { "TodoTrouble", "TodoTelescope" },
+    cmd = { "TodoTrouble", "TodoQuickFix", "TodoLocList" },
     event = "LazyFile",
     opts = {},
     -- stylua: ignore
@@ -12,8 +12,8 @@ return {
       { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
       { "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
       { "<leader>xT", "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
-      { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
-      { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+      { "<leader>st", function() Snacks.picker.pick("todo") end, desc = "Todo" },
+      { "<leader>sT", function() Snacks.picker.pick("todo", { keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
     },
   },
 }
