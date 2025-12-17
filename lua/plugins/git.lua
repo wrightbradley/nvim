fc32ab625cfbe592a57bf68ac8cccb44fb51d466
wrightbradley <bradley.wright.tech@gmail.com>
2025-12-17 12:32:32 -0500

    refactor(git): replace git-blame.nvim with built-in Snacks.git.blame_line()
    - Remove git-blame.nvim plugin (was disabled by default)
    - Add <leader>gbl keybinding for Snacks.git.blame_line()
    - Update lazy-lock.json to remove git-blame.nvim dependency
    
    This reduces plugin count by consolidating blame functionality into
    the already-installed Snacks.nvim plugin, which provides a richer
    git log view in a floating terminal.

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,13 +1,5 @@
 return {
   {
-    "f-person/git-blame.nvim",
-    event = "BufRead",
-    config = function()
-      vim.cmd("highlight default link gitblame SpecialComment")
-      vim.g.gitblame_enabled = 0
-    end,
-  },
-  {
     "dlvhdr/gh-blame.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim" },
     keys = {
