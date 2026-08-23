7e8321c3223d21b0dd4170be6199fd60429ad21d
wrightbradley <bradley.wright.tech@gmail.com>
2026-08-22 20:22:45 -0400

    chore: drop commented-out git-worktree telescope spec

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -29,46 +29,6 @@ return {
       { "<leader>gOd", "<cmd>Octo pr diff<cr>", desc = "PR Diff (Octo)" },
     },
   },
-  -- {
-  --   "polarmutex/git-worktree.nvim",
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --     "nvim-telescope/telescope.nvim",
-  --   },
-  --   config = function()
-  --     local Hooks = require("git-worktree.hooks")
-  --     require("telescope").load_extension("git_worktree")
-  --     Hooks.register(Hooks.type.SWITCH, function(path, prev_path)
-  --       local relativePath = path:gsub("^" .. os.getenv("HOME"), "")
-  --       vim.notify("Switched to ~" .. relativePath)
-  --
-  --       -- Update the current buffer
-  --       Hooks.builtins.update_current_buffer_on_switch(path, prev_path)
-  --
-  --       for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-  --         if vim.api.nvim_buf_is_loaded(buf) then
-  --           vim.api.nvim_buf_delete(buf, { force = true })
-  --         end
-  --       end
-  --     end)
-  --   end,
-  --   keys = {
-  --     {
-  --       "<leader>gws",
-  --       function()
-  --         require("telescope").extensions.git_worktree.git_worktree()
-  --       end,
-  --       desc = "Git Worktree switch",
-  --     },
-  --     {
-  --       "<leader>gwc",
-  --       function()
-  --         require("telescope").extensions.git_worktree.create_git_worktree()
-  --       end,
-  --       desc = "Create new Git Worktree",
-  --     },
-  --   },
-  -- },
   -- Worktree switching lives in lua/plugins/worktrees.lua (custom snacks
   -- picker over `git worktree list`, with persistence.nvim auto-restore).
   {
